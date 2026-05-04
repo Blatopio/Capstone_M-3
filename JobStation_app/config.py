e7d552ac0775a732f60d802271b29ef47f6850b6
Blatopio <115918210+Blatopio@users.noreply.github.com>
2026-05-05 00:44:01 +0700

    Change load_dotenv to not override existing vars

--- JobStation_app/config.py
+++ JobStation_app/config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 # ─── Langfuse ─────────────────────────────────────────────────────────────────
 from langfuse.langchain import CallbackHandler as LangfuseCallbackHandler
 
-load_dotenv()
+load_dotenv(override=False)
 
 QDRANT_URL     = os.getenv("QDRANT_URL")
 QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
